b9e0f526a7ec153dc66b22ff0c65c08110b9afce
hugo de araujo <hugo.saraujo@tutanota.com>
2023-09-26 17:30:12 -0300

    Adição de condições na função de menu

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Threading.Channels;
+
 void ExibeBannerPrograma ()
 {
     string mensagemBoasVindas = @"
@@ -21,6 +23,26 @@ void ExibeOpcoesMenu()
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir média da banda");
     Console.WriteLine("Digite 0 para sair do menu");
+
+    Console.WriteLine();
+    Console.Write("Digite a sua opção: ");
+    int opcaoEscolhida = int.Parse(Console.ReadLine()!);
+
+    switch(opcaoEscolhida)
+    {
+        case 1: Console.WriteLine("Opção 1 Escolhida...");
+            break;
+        case 2: Console.WriteLine("Opção 2 Escolhida...");
+            break;
+        case 3: Console.WriteLine("Opção 3 Escolhida...");
+            break;
+        case 4: Console.WriteLine("Opção 4 Escolhida...");
+            break;
+        case 0: Console.WriteLine("Fechando o menu...");
+            break;
+        default: Console.WriteLine("Opção Inválida!");
+            break; 
+    }
 }
 
 ExibeBannerPrograma();
