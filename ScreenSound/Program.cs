45fab0b35e6d6877874af1685754ba51a959aaac
hugo de araujo <hugo.saraujo@tutanota.com>
2023-09-26 17:11:00 -0300

    criação de função para a exibição do banner e criação de função para as mensagens de menu.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,2 +1,27 @@
-﻿string mensagemBoasVindas = "Bem vindo ao Screen Sound!!";
-Console.WriteLine(mensagemBoasVindas);
+﻿
+void ExibeBannerPrograma ()
+{
+    string mensagemBoasVindas = @"
+░██████╗░█████╗░██████╗░███████╗███╗░░██╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔════╝████╗░██║████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗██╔════╝
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░██╔██╗██║██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║╚█████╗░
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██║╚████║██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║░╚═══██╗
+██████╔╝╚█████╔╝██║░░██║███████╗██║░╚███║██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚══╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░╚═════╝░
+";
+    Console.WriteLine(mensagemBoasVindas);
+    Console.WriteLine("Boas vindas ao Screen Sound");
+
+}
+void ExibeOpcoesMenu()
+{
+    Console.WriteLine();
+    Console.WriteLine("Digite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir média da banda");
+    Console.WriteLine("Digite 0 para sair do menu");
+}
+
+ExibeBannerPrograma();
+ExibeOpcoesMenu();
