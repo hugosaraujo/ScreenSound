cbb5e9a99d5ff27f5105027ef08296e6fa0b2081
hugo de araujo <hugo.saraujo@tutanota.com>
2023-10-27 16:51:40 -0300

    Refatoração: criação de classe menu avaliar banda

---
+++ ScreenSound/Menus/MenuAvaliarBanda.cs
@@ -0,0 +1,42 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuAvaliarBanda
+{
+    void ExibirTituloOpcaoMenu(string titulo)
+    {
+        int quantidadeLetras = titulo.Length;
+        string asteriscos = string.Empty.PadLeft(quantidadeLetras + 2, '*');
+        Console.WriteLine(asteriscos);
+        Console.WriteLine($"*{titulo}*");
+        Console.WriteLine(asteriscos);
+        Console.WriteLine();
+    }
+
+    public void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.Clear();
+        ExibirTituloOpcaoMenu("Avaliar Banda");
+        Console.Write("Digite o nome da banda que deseja avaliar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        if (bandasRegistradas.ContainsKey(nomeDaBanda))
+        {
+            Banda banda = bandasRegistradas[nomeDaBanda];
+            Console.Write("Digite a nota da banda: ");
+            Avaliacao notaDaBanda = Avaliacao.Parse(Console.ReadLine()!);
+            banda.AdicionarNota((notaDaBanda));
+            Console.WriteLine($"Registrando a nota {notaDaBanda.Nota}. A {nomeDaBanda} foi avaliado(a) com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{nomeDaBanda} não foi encontrado(a)!");
+            Console.Write("Pressione alguma tecla para voltar para o Menu...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        Thread.Sleep(2000);
+        Console.Clear();
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -57,11 +57,13 @@ void ExibeOpcoesMenu()
             MostrarBandasRegistradas();
             break;
         case 4:
-            AvaliarBanda();
+            MenuAvaliarBanda opcaoAvaliarBanda = new();
+            opcaoAvaliarBanda.Executar(bandasRegistradas);
+            ExibeOpcoesMenu();
             break;
         case 5:
-            MenuExibirDetalhes exibirDetalhes = new();
-            exibirDetalhes.Executar(bandasRegistradas);
+            MenuExibirDetalhes opcaoExibirDetalhes = new();
+            opcaoExibirDetalhes.Executar(bandasRegistradas);
             ExibeOpcoesMenu();
             break;
         case 0:
@@ -109,34 +111,6 @@ void ExibirTituloOpcaoMenu(string titulo)
     Console.WriteLine($"*{titulo}*");
     Console.WriteLine(asteriscos);
     Console.WriteLine();
-}
-
-void AvaliarBanda()
-{
-    Console.Clear();
-    ExibirTituloOpcaoMenu("Avaliar Banda");
-    Console.Write("Digite o nome da banda que deseja avaliar: ");
-    string nomeDaBanda = Console.ReadLine()!;
-    if (bandasRegistradas.ContainsKey(nomeDaBanda))
-    {
-        Banda banda = bandasRegistradas[nomeDaBanda];
-        Console.Write("Digite a nota da banda: ");
-        Avaliacao notaDaBanda = Avaliacao.Parse(Console.ReadLine()!);
-        banda.AdicionarNota((notaDaBanda));
-        Console.WriteLine($"Registrando a nota {notaDaBanda.Nota}. A {nomeDaBanda} foi avaliado(a) com sucesso!");
-    }
-    else
-    {
-        Console.WriteLine();
-        Console.WriteLine($"{nomeDaBanda} não foi encontrado(a)!");
-        Console.Write("Pressione alguma tecla para voltar para o Menu...");
-        Console.ReadKey();
-        Console.Clear();
-        ExibeOpcoesMenu();
-    }
-    Thread.Sleep(2000);
-    Console.Clear();
-    ExibeOpcoesMenu();
 }
 void AvisarOpcaoInvalida()
 {
