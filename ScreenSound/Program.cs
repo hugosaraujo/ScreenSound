d3b47541affb1bc0042da30f69eafbd71fbdce26
hugo de araujo <hugo.saraujo@tutanota.com>
2024-04-18 19:35:13 -0300

    Criando o menu de opções do programa

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,9 +1,11 @@
-﻿using System.Globalization;
+﻿/*
+ * 1 - Criar uma função para exibir mensagem ao entrar na opção;
+ */
 
-exibeBanner();
-exibeMenuDeOpcoes();
+exibirBanner();
+MenuDeOpcoes();
 
-void exibeBanner()
+void exibirBanner()
 {
     string banner = @"
 ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
@@ -21,11 +23,41 @@ void exibeBanner()
 
 }
 
-void exibeMenuDeOpcoes()
+void MenuDeOpcoes()
 {
     Console.WriteLine("Digite 1 para registrar uma banda ou artista");
     Console.WriteLine("Digite 2 para mostrar artistas registrados");
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média da banda");
     Console.WriteLine("Digite 0 para fechar a aplicação");
+
+    Console.Write("Selecione uma da opções disponíveis: ");
+    int opcaoEscolhida = int.Parse(Console.ReadLine()!);
+
+    switch(opcaoEscolhida)
+    {
+        case 1: 
+            exibirMensagemDaOpcaoSelecionada(opcaoEscolhida);
+            break;
+        case 2:
+            exibirMensagemDaOpcaoSelecionada(opcaoEscolhida);
+            break;
+        case 3:
+            exibirMensagemDaOpcaoSelecionada(opcaoEscolhida);
+            break;
+        case 4:
+            exibirMensagemDaOpcaoSelecionada(opcaoEscolhida);
+            break;
+        case 0:
+            exibirMensagemDaOpcaoSelecionada(opcaoEscolhida);
+            break; Console.WriteLine("Encerrando a aplicação!!!");
+            break;
+        default: Console.WriteLine("Você selecionou uma opção inválida");
+            break;
+    }
+}
+
+void exibirMensagemDaOpcaoSelecionada(int opcao)
+{
+    Console.WriteLine($"Você selecionou a opção {opcao}");
 }
