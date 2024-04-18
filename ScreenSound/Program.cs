bfe3869bedd695ec9e83cc0b7385eddb68ff8d7c
hugo de araujo <hugo.saraujo@tutanota.com>
2024-04-18 19:12:00 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+exibeBanner();
+exibeMenuDeOpcoes();
+
+void exibeBanner()
+{
+    string banner = @"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░";
+
+    string mensagemBoasVindas = "Boas vindas ao Screen Sound";
+    
+    Console.WriteLine(banner);
+    Console.WriteLine(mensagemBoasVindas);
+    Console.WriteLine();
+
+}
+
+void exibeMenuDeOpcoes()
+{
+    Console.WriteLine("Digite 1 para registrar uma banda ou artista");
+    Console.WriteLine("Digite 2 para mostrar artistas registrados");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média da banda");
+    Console.WriteLine("Digite 0 para fechar a aplicação");
+}
