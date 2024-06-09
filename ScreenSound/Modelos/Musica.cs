adea7c6e5e0c93060eebca66659a21c328fc1826
hugo de araujo <hugo.saraujo@tutanota.com>
2024-06-09 16:10:24 -0300

    Criação da classe música

---
+++ ScreenSound/Modelos/Musica.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ScreenSound.Modelos;
+public class Musica
+{
+    public string faixa;
+    public string artista;
+    public int duracaoEmSegundos;
+    public bool disponívelNoPlano;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Titulo: {faixa} - Artista: {artista}");
+    }
+
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using ScreenSound.Modelos;
+
+Musica musica = new Musica();
+musica.faixa = "Karma Police";
+musica.artista = "RadioHead";
+
+musica.ExibirFichaTecnica();
+
+Musica ps = new Musica();
+ps.faixa = "Police Station";
+ps.artista = "Red Hot Chili Peppers";
+
+ps.ExibirFichaTecnica();
